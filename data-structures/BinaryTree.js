40f38e127aa317bc2f118be899ff98156bee1bb8
przemek <przemek@devrunner.pl>
2016-09-27 11:07:06 +0200

    BinaryTree added

---
+++ data-structures/BinaryTree.js
@@ -0,0 +1,21 @@
+/**
+ * Implementation of the binary tree using JavaScript
+ *
+ * author: Przemyslaw Jazlo <przemek@devrunner.pl>
+ *
+ */
+module.exports = (function BinaryTree(){
+
+	var root = null;
+
+	function getNode(data){
+		return {
+			data: data,
+			left: null,
+			right: null
+		};
+	}
+
+	// tbc...
+
+})();
