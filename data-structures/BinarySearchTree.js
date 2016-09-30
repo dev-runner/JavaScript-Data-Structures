a1915961a146188d8cf7a6359cb752ed14d8ffe6
przemek <przemek@devrunner.pl>
2016-09-30 12:34:50 +0200

    BST implemented

--- data-structures/BinarySearchTree.js
+++ data-structures/BinarySearchTree.js
@@ -13,7 +13,8 @@ module.exports = (function BinarySearchTree(){
 		return {
 			key: key,
 			left: null,
-			right: null
+			right: null,
+			parent: null,
 		};
 	}
 
@@ -21,18 +22,36 @@ module.exports = (function BinarySearchTree(){
 	function search(key){
 		var current_node = root;
 
-		while(current_node != null){
-			if(current_node.key === key){
-				return current_node;
-			}
-			else if(key < current_node.key){
+		while(current_node != null && current_node.key !== key){
+			if(key < current_node.key){
 				current_node = current_node.left;
-			}
-			else{
+			} else {
 				current_node = current_node.right;
 			}
 		}
-		return null; // key not found
+		return current_node;
+	}
+
+	// find minimum node
+	function searchMinNode(node){
+		while(node !== null && node.left !== null){
+			node = node.left;
+		}
+		return node;
+	}
+	function searchMin(){
+		return searchMinNode(root);
+	}
+
+	// find maximum node
+	function searchMaxNode(node){
+		while(node !== null && node.right !== null){
+			node = node.right;
+		}
+		return node;
+	}
+	function searchMax(){
+		return searchMaxNode(root);
 	}
 
 	// inserts new node to the BST
@@ -56,29 +75,98 @@ module.exports = (function BinarySearchTree(){
 					subtree = 'l';
 				}
 			}
+			var newNode = createNode(key);
+			newNode.parent = parent_node;
 			if(subtree === 'l'){
-				parent_node.left = createNode(key);
+				parent_node.left = newNode;
 			}
 			else if(subtree === 'r'){
-				parent_node.right = createNode(key);
+				parent_node.right = newNode;
 			}
 		}
 	}
 
-	function traverse_node(node){
-		if(node === null) return;
-		console.log(node.key);
-		traverse_node(node.left);
-		traverse_node(node.right);
+	// finds a successor of the node
+	function findSuccessor(node){
+		if(node.right !== null){
+			return searchMinNode(node.right);
+		}
+		var tmp_node = node.parent;
+		while(tmp_node !== null && tmp_node.left !== node){
+			node = tmp_node;
+			tmp_node = tmp_node.parent;
+		}
+		return tmp_node;
 	}
 
-	function traverse(){
-		traverse_node(root);
+	// finds predecessor of the node
+	function findPredecessor(node){
+		if(node.left !== null){
+			return searchMaxNode(node.left);
+		}
+		var tmp_node = node.parent;
+		while(tmp_node !== null && tmp_node.right !== node){
+			node = tmp_node;
+			tmp_node = tmp_node.parent;
+		}
+		return tmp_node;
+	}
+
+	// deletes node from BST
+	function removeNode(node){
+		var x = y = null;
+
+		if(node.left === null || node.right === null){
+			y = node;
+		}
+		else {
+			y = findSuccessor(node);
+		}
+		if(y.left !== null){
+			x = y.left;
+		}
+		else {
+			x = y.right;
+		}
+		if(x !== null){
+			x.parent = y.parent;
+		}
+		if(y.parent === null){
+			root = x;
+		}
+		else {
+			if(y === y.parent.left) y.parent.left = x;
+			else y.parent.right = x;
+		}
+		if(y !== node){
+			node.key = y.key;
+		}
+		return y;
+	}
+	function remove(key){
+		var node = search(key);
+		if(node !== null) {
+			removeNode(node);
+		}
+	}
+
+	// traverse bst tree in-order, f - callback function
+	function traverse_node(node, f){
+		if(node === null) return;
+		traverse_node(node.left, f);
+		f(node);
+		traverse_node(node.right,f);
+	}
+	function traverse(f){
+		traverse_node(root, f);
 	}
 
 	var publicApi = {
 		search: search,
+		searchMin: searchMin,
+		searchMax: searchMax,
 		insert: insert,
+		remove: remove,
 		traverse: traverse,
 	};
 
--- tests/bst-test.js
+++ tests/bst-test.js
@@ -1,10 +1,33 @@
 var bst = require('../data-structures/BinarySearchTree.js');
 
 bst.insert(7);
+bst.insert(3);
 bst.insert(5);
 bst.insert(13);
 bst.insert(10);
 bst.insert(15);
 
-var node = bst.search(10);
-console.log(node);
+
+// traverse bst tree (in-order)
+bst.traverse( function(node){console.log(node.key)} );
+
+// try to find node
+/*
+var found_node = bst.search(10);
+if(found_node){
+	console.log(found_node);	
+}else{
+	console.log('node not found');
+}
+
+
+var min_node = bst.searchMin();
+console.log(min_node);
+
+var max_node = bst.searchMax();
+console.log(max_node);
+*/
+console.log('Deleting 7...');
+bst.remove(7);
+bst.traverse( function(node){console.log(node.key)} );
+
