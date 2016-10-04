e96cbbd17192d13595f3db86de8ca59d21bcedc3
przemek <przemek@devrunner.pl>
2016-10-04 13:12:40 +0200

    descriptive comments in the stack implementation

--- data-structures/Stack.js
+++ data-structures/Stack.js
@@ -8,26 +8,32 @@ module.exports = (function Stack(){
 
 	var stack = [];
 
+	// inserts new element to the stack
 	function push(newElement){
 		stack.push(newElement);
 	}
 
+	// removes and returns the top element
 	function pop(){
 		return stack.pop();
 	}
 
+	// returns the top element (without removing it from the stack)
 	function peek(){
 		return stack[stack.length-1];
 	}
 
+	// returns the stack size
 	function getSize(){
 		return stack.length;
 	}
 
+	// returns true if stack is empty
 	function isEmpty(){
 		return (stack.length === 0);
 	}
 
+	// clears the stack
 	function clear(){
 		stack = [];
 	}
