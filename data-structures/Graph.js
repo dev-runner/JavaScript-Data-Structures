814790328f6910d4a7ccb3e2f9fc73c5a49a215d
przemek <przemek@devrunner.pl>
2016-10-27 22:12:56 +0200

    depth-first search path finder implemented. graph and algorithms decoupled into separate modules

--- data-structures/Graph.js
+++ data-structures/Graph.js
@@ -1,6 +1,6 @@
 /**
   * Implementation of the graph using JavaScript
-  * The graph is represented in the form of a hashed adjecency list
+  * The graph is represented in the form of adjecency list
   *
   * author: Przemyslaw Jazlo <przemek@devrunner.pl>
   *
@@ -14,10 +14,7 @@ var Graph = function(s){
 
 	// default settings
 	var default_settings = {
-		directed: true,
-		multiedge: false,
-		selfloops: false,
-		weighted: false,
+		directed: true, multiedge: false, selfloops: false, weighted: false,
 	};
 
 	// initialize settings
@@ -51,6 +48,91 @@ var Graph = function(s){
 	};
 
 
+	/** 
+	  * Returns number of vertices in the graph
+	  */
+	function numVertices(){
+		return Object.keys(vertices).length;
+	}
+
+
+	/**
+	  * Returns number of edges in the graph
+	  */
+	function numEdges(){
+		var vertexCount = 0;
+
+		for(let vKey in vertices){
+			vertexCount += Object.keys(vertices[vKey].neighbours).length;
+		}
+		if (settings['directed'] === false){
+			vertexCount /= 2;
+		}
+		return vertexCount;
+	}
+
+
+	/**
+	  * Checks if the given vertex exists
+	  */
+	function vertexExists(vKey){
+		if(typeof vertices[vKey] !== 'undefined') return true; 
+		return false;
+	}
+
+
+	/**
+	  * Checks if the edge from v1 to v2 exists
+	  */
+	function edgeExists(v1Key, v2Key){
+		if(!vertexExists(v1Key) || !vertexExists(v2Key)) return false;
+
+		if(typeof vertices[v1Key].neighbours[v2Key] !== 'undefined'){
+			return true;
+		}
+		return false;
+	}
+
+
+	/**
+	  * Returns degree (number of edges connected) of the given vertex
+	  */
+	function degree(vKey){
+		if( !vertexExists(vKey) ) throw "Vertex " + vKey + " does not exist.";
+		
+		return Object.keys(vertices[vKey].neighbours).length;
+	}
+
+
+	/**
+	  * Clears the graph (removes all vertices and edges)
+	  */
+	function clear(){
+		vertices = {};
+	}
+
+
+	/**
+	  * Returns the string representation of the graph's adjecency list
+	  */
+	function toString(){
+		var s = '';
+		for(let vKey in vertices){
+			let v = vertices[vKey];
+			var numEdges = Object.keys(v.neighbours).length, i=0;
+			s += vKey;
+			s += ' --> [';
+			for (let eKey in v.neighbours){
+				s += eKey;
+				++i;
+				if(i < numEdges) s += ', ';
+			}
+			s += ']\n';
+		}
+		return s;
+	}
+
+
 	/**
 	  * Inserts new vertex to the graph, key must be unique
 	  */
@@ -62,12 +144,13 @@ var Graph = function(s){
 		}
 
 		// check if vertex already exists
-		if(key in vertices){
-			throw "Vertex with key = '" + key + "' already exists.";
+		if( vertexExists(key) ) {
+			throw "Vertex " + key + "' already exists.";
 		}
 
 		// push to the vertices hash map
 		vertices[key] = new Vertex(key, data);
+		return true;
 	}
 
 
@@ -75,12 +158,11 @@ var Graph = function(s){
 	  * Removes vertex from the graph
 	  */
 	function deleteVertex(vKey){
+		
 		if(typeof vKey === 'undefined'){
 			throw "Must privide vertex key.";
 		}
-		if((vKey in vertices) === false){
-			throw "Vertex " + vKey + " does not exist.";
-		}
+		if( !vertexExists(vKey) ) throw "Vertex " + vKey + " does not exist.";
 
 		// delete vertex from vertices object
 		delete vertices[vKey];
@@ -97,12 +179,9 @@ var Graph = function(s){
 	  */
 	function addEdge(v1Key, v2Key, weight){
 
-		if(typeof vertices[v1Key] === 'undefined'){
-			throw "Vertex " + v1Key + " does not exist.";
-		}
-		if(typeof vertices[v2Key] === 'undefined'){
-			throw "Vertex " + v2Key + " does not exist.";
-		}
+		if( !vertexExists(v1Key) ) throw "Vertex " + v1Key + " does not exist.";
+		
+		if( !vertexExists(v2Key) ) throw "Vertex " + v2Key + " does not exist.";
 
 		// handle selfloops
 		if(v1Key === v2Key && settings['selfloops'] === false){
@@ -110,7 +189,7 @@ var Graph = function(s){
 		}
 
 		// handle multiedges
-		if(areConnected(v1Key, v2Key) && settings['multiedge'] === false){
+		if(edgeExists(v1Key, v2Key) && settings['multiedge'] === false){
 			throw "Multiedges are not allowed.";
 		}
 
@@ -129,19 +208,16 @@ var Graph = function(s){
 		if(settings['directed'] === false){
 			vertices[v2Key].addNeighbour(v1Key, weight);
 		}
-
 	}
+
 
 	/**
 	  * Deletes an edge
 	  */
 	function deleteEdge(v1Key, v2Key){
-		if(typeof vertices[v1Key] === 'undefined'){
-			throw "Vertex " + v1Key + " does not exist.";
-		}
-		if(typeof vertices[v2Key] === 'undefined'){
-			throw "Vertex " + v2Key + " does not exist.";
-		}
+		
+		if( !vertexExists(v1Key) ) throw "Vertex " + v1Key + " does not exist.";
+		if( !vertexExists(v2Key) ) throw "Vertex " + v2Key + " does not exist.";
 
 		if(typeof vertices[v1Key].neighbours[v2Key] !== 'undefined'){
 			
@@ -159,40 +235,130 @@ var Graph = function(s){
 
 
 	/**
-	  * Checks if two nodes are connected with an edge
+	  * Finds all adjacent vertices for the given vertex
 	  */
-	function areConnected(v1Key, v2Key){
+	function getAdjacentVertices(vKey){
 
-		if(typeof vertices[v1Key] === 'undefined'){
-			throw "Vertex " + v1Key + " does not exist.";
-		}
-		if(typeof vertices[v2Key] === 'undefined'){
-			throw "Vertex " + v2Key + " does not exist.";
-		}
+		if( !vertexExists(vKey) ) throw "Vertex " + vKey + " does not exist.";
 
-		if(typeof vertices[v1Key].neighbours[v2Key] !== 'undefined'){
-			return true;
-		}
-		return false;
+		return vertices[vKey].neighbours;
 	}
 
 
 	/**
-	  * Finds all adjacent vertices for the given vertex
+	  * Returns an array of graphs vertices' keys
 	  */
-	function getAdjacentVertices(vKey){
-		
-		if(typeof vertices[vKey] === 'undefined'){
-			throw "Vertex " + vKey + " does not exist.";
-		}
-
-		return vertices[vKey].neighbours;
+	function getVertices(){
+		return Object.keys(vertices);
 	}
+
+
+	/**
+	  * Graph public API
+	  */
+	var publicApi = {
+		numVertices: numVertices,
+		numEdges: numEdges,
+		vertexExists: vertexExists,
+		edgeExists: edgeExists,
+		degree: degree,
+		clear: clear,
+		toString: toString,
+		addVertex: addVertex,
+		deleteVertex: deleteVertex,
+		addEdge: addEdge,
+		deleteEdge: deleteEdge,
+		getAdjacentVertices: getAdjacentVertices,
+		getVertices: getVertices,
+	};
+
+	return publicApi;
+};
+
+
+/**
+  * DFS path finder
+  */
+var DepthFirstPaths = function(graph, startVertex){
+
+	"use strict";
+
+	if(typeof graph === 'undefined' || typeof graph != 'object') throw "Provide a graph object.";
+	if(typeof startVertex === 'undefined' || !graph.vertexExists(startVertex)) throw "Provide an existing vertex key.";
+
+	var visited = {};
+	var edgeTo = {};
+
+	// run DFS to find the paths
+	dfs(startVertex);
+
+
+	/**
+	  * Recursive DFS traversal
+	  */
+	function dfs(v){
+
+		visited[v] = true;
+		var adjList = graph.getAdjacentVertices(v);
+		
+		for(let w in adjList){
+			if(typeof visited[w] === 'undefined'){
+				dfs(w);
+				edgeTo[w] = v;
+			}
+		}
+	}
+
+
+	/**
+	  * Returns true if the given vertex is connected to the start vertex
+	  */
+	function isConnected(v){
+		if( !graph.vertexExists(v) ) throw "Vertex " + v + " does not exist.";
+		return (typeof visited[v] !== 'undefined' && visited[v] == true);
+	}
+
+
+	/**
+	  * Returns a DFS path from start vertex to vertex v
+	  */
+	function pathTo(v){
+		
+		if( !isConnected(v) ) return null;
+
+		var stack = [];
+		for(let x=v; x != startVertex; x = edgeTo[x]){
+			stack.push(x);
+		}
+		stack.push(startVertex);
+		return stack;
+	}
+
+
+	var publicApi = {
+		isConnected: isConnected,
+		pathTo: pathTo,
+	};
+
+	return publicApi;
+};
+
+
+/**
+  * Graph traversal algorithms
+  */
+var Traversal = function(graph){
+	
+	"use strict";
+
 
 	/**
 	  * Depth-First Search traversal of the graph
 	  */
 	function traverseDFS(vKey, f) {
+
+		if( !vertexExists(vKey) ) throw "Vertex " + vKey + " does not exist.";
+		f = (typeof f === 'function') ? f : function(v) { console.log(v.key); };
 
 		// first, set all vertices and edges as unvisited
 		for(let key in vertices){
@@ -212,14 +378,14 @@ var Graph = function(s){
 			
 			// stack keeps vertices that have been visited and which have unvisited neighbours
 			var stack = [];
-			stack.push(vertices[vKey])
+			stack.push(startVertex);
 
 			while(stack.length > 0){
 				
 				// take vertex v from top of stack
 				let v = stack[stack.length-1];
 
-				// take next unvisited edge coming from vertex v
+				// take next unvisited edge starting at vertex v
 				let endVKey = v.unvisitedEdges.shift();
 				let endVertex = vertices[endVKey];
 
@@ -233,38 +399,24 @@ var Graph = function(s){
 					f(endVertex); // visit the vertex
 					endVertex.visited = true; // set as visited
 
-					if(endVertex.unvisited.length > 0) {
+					if(endVertex.unvisitedEdges.length > 0) {
 						stack.push(endVertex);
 					}
 				}
+
 			}
 		}
 	}
 
-
-	/**
-	  * Breadth-First Search traversal of the graph
-	  */
-	function traverseBFS(vKey, f){
-		
-		// todo: implement
-
-	}
-
-
-	var publicApi = {
-		addVertex: addVertex,
-		deleteVertex: deleteVertex,
-		addEdge: addEdge,
-		deleteEdge: deleteEdge,
-		areConnected: areConnected,
-		getAdjacentVertices: getAdjacentVertices,
-		traverseDFS: traverseDFS,
-		traverseBFS: traverseBFS,
-	};
-	
+	var publicApi = {};
 	return publicApi;
 
 };
 
-module.exports = Graph;
+
+module.exports = {
+	Graph: Graph,
+	Traversal: Traversal,
+	DepthFirstPaths: DepthFirstPaths,
+};
+
